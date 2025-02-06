64546d22aa5b0aed0f799f111878d411f6331e1b
Sara Haptonstall <112976778+sarahaptumich@users.noreply.github.com>
2025-02-06 13:55:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Owl 1.0", page_icon=":owl:")
 st.title("Owl 1.0")
 
 # Sidebar settings
-st.sidebar.image("humanitarian_bot.jpeg")
+st.sidebar.image("owl_logo.jpg")
 st.sidebar.header("Settings")
 
 # Model selection and configuration
